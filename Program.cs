126f9095acb9eb42c5da9f057adc52c9601a11fb
Evgenij Zavgorodnij <razorval99@gmail.com>
2018-03-16 23:05:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Kakaya_To_Xernia
 {
     class Program
     {
-        static int number1 = 0;
-        static int number2 = 0;
-        static int number3 = 0;
-        static bool triger = false;
+        static int number1 = 0; static int number2 = 0; static int number3 = 0; static bool triger = false;
         static void Main(string[] args)
         {
             //Религия мне не позволяет использовать паскаль!
+            //Даже ТА САМАЯ ЛЕРА ИЗ ИСОВ НЕ СМОЖЕТ ЕЁ СЛОМАТЬ!
             do
             {
                 triger = false;
@@ -25,13 +19,21 @@ namespace Kakaya_To_Xernia
                     Console.Write("Введите третье число="); number3 = int.Parse(Console.ReadLine());
                     if (number1<=0 || number2<=0 || number3<=0 || number1>=10 ||number2>=10 ||number3>=10) throw new FormatException("Введи нормальное число");
                 }
-                
+                catch (System.ArgumentNullException e)
+                {
+                    Console.WriteLine("Баалляяяя!!! Мне больно!");
+                    triger = true;
+                }
                 catch (System.FormatException e)
                 {
                     Console.WriteLine("Баалляяяя!!! Мне больно!");
                     triger = true;
                 }
-
+                catch (System.OverflowException e)
+                {
+                    Console.WriteLine("Баалляяяя!!! Мне больно!");
+                    triger = true;
+                }
             } while (triger);
            
             Console.WriteLine("////////////////////////////////////////////////////////////");
